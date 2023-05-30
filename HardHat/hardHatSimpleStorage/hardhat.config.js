96e20705a44d3970270f1ba236f21188171c5a69
maancham <houmaanch@gmail.com>
2023-05-30 16:15:37 -0300

    hardhat simplestorage verification added

--- HardHat/hardHatSimpleStorage/hardhat.config.js
+++ HardHat/hardHatSimpleStorage/hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,5 +14,8 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
--- HardHat/hardHatSimpleStorage/scripts/deploy.js
+++ HardHat/hardHatSimpleStorage/scripts/deploy.js
@@ -8,7 +8,38 @@ async function main() {
   const simpleStorage = await SimpleStorageFactory.deploy();
   await simpleStorage.deployed();
   console.log(`Deployed contract to: ${simpleStorage.address}`);
+  if (process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
+    // for for some blocks until verification
+    await simpleStorage.deployTransaction.wait(5);
+    await verify(simpleStorage.address, []);
+  }
+
+  const firstValue = await simpleStorage.retrieve();
+  console.log(`The first Value is: ${firstValue}`);
+
+  const transactionResponse = await simpleStorage.storeFav(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is: ${updatedValue}`);
 }
+
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
 
 // main
 main()
@@ -17,3 +48,5 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+// yarn hardhat run scripts/deploy.js --network sepolia
