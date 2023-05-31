f71c981e09e892786e5eca6072d93e348e35a66c
maancham <houmaanch@gmail.com>
2023-05-31 11:49:34 -0300

    hardhat simplestorage completed

--- HardHat/hardHatSimpleStorage/hardhat.config.js
+++ HardHat/hardHatSimpleStorage/hardhat.config.js
@@ -1,9 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 require("dotenv").config();
+require("./tasks/blockNumber");
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,8 +18,18 @@ module.exports = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [PRIVATE_KEY],
     },
+    localhost: {
+      url: "http://localhost:8545",
+      //accounts are given by hardhat
+      chainId: 31337,
+    },
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
 };
